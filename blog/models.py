c949cd2f688d75e4f5a804d0f5485a4d3f4c4d6c
jiwonsis@naver.com <crister86>
2017-08-08 14:33:47 +0900

    - chapter.2 Blog Application 생성 및 DB연동

---
+++ blog/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super(PublishedManager, self).get_queryset().filter(status='published')
+
+class Post(models.Model):
+    STATUS_CHOICES = (
+        ('draft', 'Draft'),
+        ('published', 'Published'),
+    )
+    title = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250, unique_for_date='publish')
+    author = models.ForeignKey(User, related_name='blog_post')
+    body = models.TextField()
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
+
+    objects = models.Manager()  # 디폴트 메니져
+    published = PublishedManager()  # 블로그 모델에서 파생된 메니져
+
+    class Meta:
+        ordering = ('-publish',)
+
+    def __str__(self):
+        return self.title
