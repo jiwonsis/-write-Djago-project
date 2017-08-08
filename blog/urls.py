e9cffb43052568cac47f3af156317c1655ef9a78
jiwonsis@naver.com <crister86>
2017-08-08 13:59:31 +0900

    - chapter.3 Blog 어플리케이션 화면 개발

---
+++ blog/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+    # post 리스트 뷰 (root)
+    url(r'^$', views.post_list, name='post_list'),
+    # post 디테일 뷰 (/년(4)/월(2)/일(2)/포스트 슬러그명)
+    url(r'^(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/(?P<post>[-\w]+)/$',
+        views.post_detail, name='post_detail'),
+]
